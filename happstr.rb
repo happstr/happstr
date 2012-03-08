3194c47916ebf42ff28414cceeda4cac0fed15e2
Freenerd <nospam@freenerd.de>
2012-03-07 20:29:20 -0600

    Add Beta

--- happstr.rb
+++ happstr.rb
@@ -36,6 +36,10 @@ class Happstr < Sinatra::Base
     send_file File.join(settings.public_folder, 'html/index.html')
   end
 
+  get '/beta' do
+    send_file File.join(settings.public_folder, 'html/beta.html')
+  end
+
   get '/api/checkins' do
     # validate parameters
     lat = params[:lat]
